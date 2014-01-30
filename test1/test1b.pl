17c517d9bc7072f3f175c906d9700f55700ebad1
iceshard <mailings@beitzel.org>
2014-01-30 18:02:34 -0500

    feature3 - take 1

--- test1/test1b.pl
+++ test1/test1b.pl
@@ -5,7 +5,13 @@ use strict;
 
 my $j = 99;
 
-for my $i (0..10) {
-	print "b...",$i," featuer 1",$/;
+sub feature3
+{
+	print "this is code for feature3\n";
 }
 
+for my $i (0..10) {
+	print "b...",$i," feature 3",$/;
+}
+
+feature3();
